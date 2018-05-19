8b86fa692e8fd01e1f2fd0cc700afbc8a3323206
Bogdan <none>
2018-05-19 15:40:12 +0300

    canvas loader w/o threejs

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -93,3 +93,30 @@ export const getScrollbarWidth = () => {
 
   return widthNoScroll - widthWithScroll;
 };
+
+export const createShader = (gl, type, source) => {
+  const shader = gl.createShader(type);
+  gl.shaderSource(shader, source);
+  gl.compileShader(shader);
+  const success = gl.getShaderParameter(shader, gl.COMPILE_STATUS);
+  if (success) {
+    return shader;
+  }
+
+  console.log(gl.getShaderInfoLog(shader));
+  gl.deleteShader(shader);
+};
+
+export const createProgram = (gl, vertexShader, fragmentShader) => {
+  const program = gl.createProgram();
+  gl.attachShader(program, vertexShader);
+  gl.attachShader(program, fragmentShader);
+  gl.linkProgram(program);
+  const success = gl.getProgramParameter(program, gl.LINK_STATUS);
+  if (success) {
+    return program;
+  }
+
+  console.log(gl.getProgramInfoLog(program));
+  gl.deleteProgram(program);
+};
